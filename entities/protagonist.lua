7ffb53f131db9eabb1ef2859acdb25ff8d3e83b0
Braulio Viveros <trotopype.dev@gmail.com>
2025-06-26 21:19:02 -0700

    creation of protagonist and README

---
+++ entities/protagonist.lua
@@ -0,0 +1,21 @@
+local protagonist = {}
+
+function walk(direction) 
+
+end
+
+
+-- only jump if player is touching the ground unless powerup obtained
+function jump(powerup, direction)
+
+end
+
+
+function climb(direction)
+
+end
+
+
+function jump_from_ladder(direction)
+
+end
